4b57766f219515f6a9f06ef6258bacb9d1cd97fe
Matthias Auer <mail@matthias-auer.net>
2016-07-24 23:02:20 +0200

    added a mechanism to allow scaling

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,19 +1,45 @@
 package;
 
-
 import net.matthiasauer.di.Component;
 import net.matthiasauer.di.ISystem;
 import net.matthiasauer.tictactoe.view.SVGExample;
 import openfl.display.Sprite;
 import net.matthiasauer.di.System;
+import flash.events.Event;
+import openfl.display.StageScaleMode;
+import openfl.Lib;
 
 class Main extends Sprite {
+	var originalWidth:Float;
+	var originalHeight:Float;
+	
+	public function resize (newWidth:Int, newHeight:Int):Void {
+		var maxWidthScale = newWidth / originalWidth;
+		var maxHeightScale = newHeight / originalHeight;
+		var newScale = Math.min(maxWidthScale, maxHeightScale);
+		
+		this.stage.scaleX = newScale;
+		this.stage.scaleY = newScale;
+		
+		this.x = newWidth / (newScale * 2);
+	}
 	
 	public function new () {
 		super ();
 		
 		this.addChild(new SVGExample());
+		
+		this.originalHeight = stage.stageHeight;
+		this.originalWidth = stage.stageWidth;
+		
+		stage.addEventListener (Event.RESIZE, stage_onResize);
 	}
 	
 	
+	private function stage_onResize (event:Event):Void {
+		var newWidth:Int = stage.stageWidth;
+		var newHeight:Int = stage.stageHeight;
+
+		this.resize(newWidth, newHeight);
+	}
 }
--- Source/net/matthiasauer/tictactoe/view/SVGExample.hx
+++ Source/net/matthiasauer/tictactoe/view/SVGExample.hx
@@ -17,9 +17,17 @@ class SVGExample extends Sprite
 	{
 		super();
 
-		svg = new SVG(Assets.getText("assets/awesome_tiger.svg"));
+		svg = new SVG(Assets.getText("assets/game_board.svg"));
 		shape = new Shape();
 		svg.render(shape.graphics);
 		addChild(shape);
+		
+		// position the element
+		this.shape.scaleX = 0.9;
+		this.shape.scaleY = 0.9;
+		this.shape.y = 20;
+		
+		// center the element
+		this.shape.x = -this.shape.width / 2;
 	}
 }
